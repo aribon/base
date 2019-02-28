9611cb5d8f7dcd24a3fa60e20788aca51404c0d7
Unknown <anthony.ribon@gmail.com>
2019-02-28 01:08:48 +0100

    fix BaseFragment

--- src/main/java/me/aribon/base/android/base/BaseFragment.java
+++ src/main/java/me/aribon/base/android/base/BaseFragment.java
@@ -1,5 +1,6 @@
 package me.aribon.base.android.base;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
